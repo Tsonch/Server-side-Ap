7d4e2ca39d42d4a9c29f81898f9cf6c20211d714
Pavel Fedoseev <pashagame2004@mail.ru>
2024-06-14 01:11:37 +0500

    lb6

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Log;
 
 class GitController extends Controller
 {
-    public function UpdateGit(Request $request) {
-
+    public function UpdateGit(Request $request)
+    {
         $secret_key_user = $request->input('secret_key');
         $secret_key_env = env('SECRET_KEY');
 
@@ -21,30 +21,30 @@ class GitController extends Controller
             $lock = Cache::lock('update in process', 300);
             if ($lock->get()) {
                 try {
+                    $path = "C:\Program Files\Git\bin\git.exe";
                     Log::info('Дата: ' . now() . ' IP:' . $request->ip());
                     Log::info('Branch checkout:');
-                    $output = shell_exec('git checkout main');
+
+                    $output = shell_exec("\"$path\" checkout main");
                     Log::info($output);
                     Log::info('Fetch All:');
-                    $output = shell_exec('git fetch --all');
+                    $output = shell_exec("\"$path\" fetch --all");
                     Log::info($output);
                     Log::info('Reset Hard:');
-                    $output = shell_exec('git reset --hard origin/main');
+                    $output = shell_exec("\"$path\" reset --hard origin/main");
                     Log::info($output);
                     Log::info('Pull origin main:');
-                    $output = shell_exec('git pull origin main');
+                    $output = shell_exec("\"$path\" pull origin main");
                     Log::info($output);
                     $lock->release();
                     return response()->json(['message' => 'Code updated'], 200);
-                }
-                catch (\Exception $e) {
+                } catch (\Exception $e) {
                     Log::error('Error: ' . $e->getMessage());
                     $lock->release();
                     return response()->json($e->getMessage(), 500);
                 }
             }
-        }
-        else {
+        } else {
             return response()->json(['error' => 'Incorrect secret key'], 401);
         }
     }
